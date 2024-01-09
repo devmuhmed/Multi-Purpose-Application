15f47d09e1a94eebfeb3ad57af1e303e92af5fe9
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-09 13:26:45 +0200

    add appointment template create appointment and client model

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->safeEmail(),
+        ];
+    }
+}

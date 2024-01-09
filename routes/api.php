280e19f593adf4a2c8114fc6266dcba022336ebc
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-09 23:44:23 +0200

    enhance route fix edit bug

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'App\\Http\\Controllers\\Api\\Admin'],function() {
-    Route::get('users','UserController@index');
-    Route::post('users','UserController@store');
-    Route::patch('users/{user}/change-role','UserController@changeRole');
-    Route::put('users/{user}','UserController@update');
-    Route::delete('users/{user}','UserController@destroy');
+    Route::apiResource('users','UserController');
     Route::delete('users','UserController@bulkDelete');
-    Route::get('users/search','UserController@search');
+    Route::patch('users/{user}/change-role','UserController@changeRole');
 
     Route::get('appointments','AppointmentController@index');
     Route::post('appointments/create','AppointmentController@store');
